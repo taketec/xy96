ded01049bd2d2c1e0e81efa50327b25c626e0dff
taketec <shailupandit293@gmail.com>
2024-04-13 21:17:03 +0530

    setting auth up

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -15,7 +15,7 @@ router.get('/auth/valid', Auth, validUser);
 
 //all of the endpoints required for authentication are managed in the user controller and user routes
 //all of the auth endpoints will be at /auth
-//  /register will take in firstname, lastname, email, password
+//  /register will take in firstname, lastname, email, password; returns a jwt token
 //  /login will take email and password and return a jwt token
 //  /valid user will take in the userid and check if the user is valid or not
 
