d1a611d8d79524aa88989d21789217807dd03050
taketec <shailupandit293@gmail.com>
2024-04-13 13:00:37 +0530

    added auth funtionality to the backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,7 @@
 import {
     register,
     login,
-    validUser,
-    logout,
+    validUser
   } from '../controllers/user.js';
   import express from 'express';
 
@@ -13,13 +12,11 @@ const router = express.Router();
 router.post('/auth/register', register);
 router.post('/auth/login', login);
 router.get('/auth/valid', Auth, validUser);
-router.get('/auth/logout', Auth, logout);
 
 //all of the endpoints required for authentication are managed in the user controller and user routes
 //all of the auth endpoints will be at /auth
 //  /register will take in firstname, lastname, email, password
 //  /login will take email and password and return a jwt token
 //  /valid user will take in the userid and check if the user is valid or not
-//  /logout used for logging out (not required but nice to have)
 
 export default router;
