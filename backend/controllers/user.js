7b28073cd40f70b6fdd7e733dde8f025181e61d4
taketec <shailupandit293@gmail.com>
2024-04-13 21:15:34 +0530

    setting auth up

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -98,7 +98,5 @@ export const register = async (req, res) => {
   //   }
   // };
   
-  export const logout = (req, res) => {
-    req.rootUser.tokens = req.rootUser.tokens.filter((e) => e.token != req.token);
-  };
 
+  
